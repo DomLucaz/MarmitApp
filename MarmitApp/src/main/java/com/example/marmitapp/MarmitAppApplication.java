acd2bd0cbc14d5973a65420a79164f83a2c6b02c
Lucas Figueiredo <91294318+DomLucaz@users.noreply.github.com>
2025-09-14 23:10:15 -0300

    init commit

---
+++ MarmitApp/src/main/java/com/example/marmitapp/MarmitAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.marmitapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarmitAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarmitAppApplication.class, args);
+    }
+
+}
---
+++ MarmitApp/src/test/java/com/example/marmitapp/MarmitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marmitapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarmitAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
